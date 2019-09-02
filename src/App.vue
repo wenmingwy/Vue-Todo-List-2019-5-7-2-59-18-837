719cfadbc22398dba1a118486907ce6693d30bc4
wenmingwy <2377812120@qq.com>
2019-09-02 23:50:25 +0800

    wm02

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+      <div>
+         <h2>Jquery To Do List</h2>
+        <!-- <h2 style="text-align:left">Jquery To Do List</h2> -->
+          Simple Todo List with adding and filter by diff status.
+         <div> 
+            <input class="input-text" type="text" name="add" />
+            <div id="button">Add</div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    todolist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
